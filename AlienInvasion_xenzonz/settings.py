40c69cc0d3d9377dd0476880254711a1601ef2ed
sam cocquyt <samcocquyt04@gmail.com>
2026-04-13 07:07:24 -0400

    added game from unit 12 participation activity

---
+++ AlienInvasion_xenzonz/settings.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+class Settings:
+    
+    def __init__(self):
+        self.name: str = 'Alien Invasion'
+        self.screen_w = 1200
+        self.screen_h = 800
+        self.FPS = 60
+        self.bg_file = Path.cwd() / 'Alieninvasion_xenzonz' / 'Assets' / 'images' / 'Starbasesnow.png'
+        
+        self.ship_file = Path.cwd() /'Alieninvasion_xenzonz' / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_w = 40
+        self.ship_h = 60
+        self.ship_speed = 5
+
+        self.bullet_file = Path.cwd() / 'Alieninvasion_xenzonz' / 'Assets' / 'images' / 'laserBlast.png'
+        self.laser_sound = Path.cwd() / 'Alieninvasion_xenzonz' / 'Assets' / 'sound' / 'laser.mp3'
+        self.bullet_speed = 7
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_amount = 5
---
+++ AlienInvasion_xenzonz/ship.py
@@ -0,0 +1,56 @@
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
+
+
+
+class Ship:
+
+    
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
+        self.game = game
+        self.settings = game.settings
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
+
+        self.image = pygame.image.load(self.settings.ship_file)
+        self.image = pygame.transform.scale(self.image, 
+            (self.settings.ship_w, self.settings.ship_h)
+            )
+        
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.boundaries.midbottom
+
+        self.moving_right = False
+        self.moving_left = False
+        self.x = float(self.rect.x)
+
+        self.arsenal = arsenal
+
+
+    def update(self):
+        #updating the position of the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
+        temp_speed = self.settings.ship_speed
+        
+        if self.moving_right and self.rect.right < self.boundaries.right:
+            self.x += temp_speed
+        if self.moving_left and self.rect.left > self.boundaries.left:
+            self.x -= temp_speed
+
+        self.rect.x = self.x
+
+
+    def draw(self):
+        self.arsenal.draw()
+        self.screen.blit(self.image, self.rect)
+
+    
+    def fire(self):
+        return self.arsenal.fire_bullet()
